542d14db8bf7f4c2669e5a7dae68194138970668
cherqui ahmed amine <cherq@Megan_bias.cherqui2003@gmail.com>
2025-12-02 23:03:15 +0100

    succes des tests de l'exercice 1

--- src/test/java/org/example/CalculatriceAireTest.java
+++ src/test/java/org/example/CalculatriceAireTest.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,6 +16,32 @@ class CalculatriceAireTest {
 
     @Test
     void testAire() {
+        List<IForme> formes = Arrays.asList(
+                new Cercle(2.0),       // aire = 12.56
+                new Rectangle(2, 4)    // aire = 8
+        );
+
         assertEquals(20.56, CalculatriceAire.aire(formes), 0.01);
     }
+
+    @Test
+    void testAireCarreAvecMockito() {
+        // Mock de IForme (simule un carré)
+        IForme carre = Mockito.mock(IForme.class);
+
+        // Comportement attendu : aire = 4
+        Mockito.when(carre.aire()).thenReturn(4.0);
+
+        // Calcul
+        List<IForme> formes = Arrays.asList(carre);
+        double resultat = CalculatriceAire.aire(formes);
+
+        // Vérification du résultat
+        assertEquals(4.0, resultat, 0.01);
+
+        // Vérifier que la méthode aire() a été appelée
+        Mockito.verify(carre).aire();
+    }
+
+
 }
